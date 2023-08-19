4bcc4f215462ad499dbacf1e7e787f3a05d9fbae
chyoungseok <chyoungseok@hanyang.ac.kr>
2023-08-19 21:11:26 +0900

    Re-examination & Smin of scheme2
    - Smin from both scheme1 and scheme2 need to be organized as independent
      functions

--- modules/stability_scheme1.py
+++ modules/stability_scheme1.py
@@ -47,18 +47,19 @@ class stability():
         ax.set_xticks(range(2,max_K+1))
             
     
-    def __init__(self, org_data, K=2, B=10, sim_method="euclidean", clst_alg='kmeans') -> None:
+    def __init__(self, org_data, K=2, B=10, B2O_mapping_method='jaccard', clst_alg='kmeans') -> None:
         self.org_data = org_data
         self.K = K
         self.B = B
-        self.sim_method = sim_method
+        # self.sim_method = sim_method
         self.clst_alg = clst_alg
+        self.B2O_mapping_method = B2O_mapping_method
 
         # Clustering for the original data
         _orgClustering = clustering_algs(data = org_data,
                                          clst_alg = clst_alg,
-                                         K = K,
-                                         sim_method = sim_method)
+                                         K = K)
+                                        #  sim_method = sim_method)
         
         # Clustering for the each of the bootstrapped data
         list_bootClustering = []
@@ -73,17 +74,18 @@ class stability():
             _bootClustering = clustering_algs(data = boot_data,
                                               clst_alg = clst_alg,
                                               K = K,
-                                              sim_method = sim_method,
                                               random_state = b)
+                                            #   sim_method = sim_method,
+                                              
             
             # mapping B_centers into O_centers
             mapped_center = map_B_center_to_O_center(org_data=org_data,
                                                      O_centers = _orgClustering.center,
                                                      O_lables = _orgClustering.labels,
                                                      B_centers = _bootClustering.center,
-                                                     sim_method = sim_method,
+                                                     B2O_mapping_method = B2O_mapping_method,
                                                      nk = K)
-            _bootClustering.o2b_center = mapped_center            
+            _bootClustering.B2O_center = mapped_center # B2O_center update          
             list_bootClustering.append(_bootClustering)
             
             o2b_labels = cdist(_bootClustering.center, org_data, metric='euclidean').argmin(axis=0) # original data를 bootClustering의 center에 mapping
@@ -110,10 +112,11 @@ class stability():
                 stability_matrix_jaccard[b, i] = cal_jaccard(org_set, o2b_set)
                 
             
-            # Cluster-wise jaccard based stability
+            # Smin (scheme1)
+            # Cluster-wise jaccard based stability 
             for k in range(K):
                 cluster_wise_similarity = 0
-                org_set = set(idx[_orgClustering.labels == k]) # k 번째 cluster에 포함된 모든 sample들의 index
+                org_set = set(idx[_orgClustering.labels == k]) # k 번째 org cluster에 포함된 모든 sample들의 index
                 for idx_for_tempK in org_set: # org_list의 모든 sample에 대한 반복문
                     temp_label = o2b_labels[idx_for_tempK] # temp sample의 o2b_label
                     o2b_set = set(idx[o2b_labels == temp_label]) # temp sample이 포함되어 있는 o2b cluster의 모든 sample의 index
@@ -122,7 +125,40 @@ class stability():
                 
                 stability_matrix_cluster_wise_jaccard[b, k] = cluster_wise_similarity/len(org_set)
 
-            Smin = np.mean(np.min(stability_matrix_cluster_wise_jaccard, axis=1)) # calculate Smin
+        Smin_scheme1 = np.mean(np.min(stability_matrix_cluster_wise_jaccard, axis=1)) # calculate Smin
+
+        # Smin (scheme2)
+        # reference clustering을 original clustering 뿐만 아니라, 각각의 bootstrapping clustering에도 적용
+        Smin_scheme2_for_each_ref = np.empty((B+1, 1)) # reference clustering을 달리하면서 계산한 각 Smin을 기록 --> 마지막에 이들을 평균내서 Smin_scheme2를 구함
+        total_clustering = [_orgClustering] + list_bootClustering
+
+        for r in range(B+1):
+            # total_clustering의 모든 요소가 reference_clustering의 후보임 --> 각 ref에 대하여 반복
+            ref_clustering = total_clustering[r] # reference clustering 지정
+            stability_matrix_cluster_wise_jaccard_scheme2 = np.empty((B, K)) # 현재 ref_clustering에 대한 cluster wise stability를 저장할 matrix
+
+            b_iter = 0
+            for b in range(B+1):
+                if b == r:
+                    continue
+                
+                r2b_labels = cdist(total_clustering[b].center, total_clustering[r].data, metric='euclidean').argmin(axis=0) # reference data를 bootClustering의 center에 mapping
+
+                # Cluster-wise jaccard based stability (comparison between ref_clustering vs. total_clustering[b])
+                for k in range(K):
+                    cluster_wise_similarity = 0
+                    ref_set = set(idx[ref_clustering.labels == k]) # ref_clustering의 k번째 cluster에 포함된 모든 sample들의 index
+                        
+                    for idx_for_tempK in ref_set: # ref_idx의 모든 sample에 대한 반복문
+                        temp_label = r2b_labels[idx_for_tempK] # temp sample의 r2b_label
+                        r2b_set = set(idx[r2b_labels == temp_label]) # temp sample이 포함되어 있는 r2b cluster의 모든 sample의 index
+                        cluster_wise_similarity += cal_jaccard(ref_set, r2b_set)
+                    stability_matrix_cluster_wise_jaccard_scheme2[b_iter, k] = cluster_wise_similarity/len(ref_set)
+                b_iter += 1
+            Smin_scheme2_for_each_ref[r] = np.mean(np.min(stability_matrix_cluster_wise_jaccard_scheme2, axis=1)) # calculate Smin for each reference clustering
+        Smin_scheme2 = np.mean(Smin_scheme2_for_each_ref) # Smin_scheme2 calculated by averaging all the Smin of reference clustering
+
+
             
         self._orgClustering = _orgClustering
         self.list_bootClustering = list_bootClustering
@@ -131,4 +167,5 @@ class stability():
         self.stability_matrix_cluster_wise_jaccard = stability_matrix_cluster_wise_jaccard
         self.naive_stabs = self.getStabilities(stability_matrix_naive, _orgClustering, K=K)
         self.jaccard_stabs = self.getStabilities(stability_matrix_jaccard, _orgClustering, K=K)
-        self.Smin = Smin
+        self.Smin_scheme1 = Smin_scheme1
+        self.Smin_scheme2 = Smin_scheme2
--- modules/utils.py
+++ modules/utils.py
@@ -3,20 +3,24 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
 
-def explore_new_label(distances_rest, nk):
+def explore_new_label(dist_btween_centers, nk):
+    """
+
+    """
+    
     all_false = [False] * nk
     new_label = np.ones(nk) * -1 # B clustering의 기존 label을 어떤 label로 바꿔야 할지에 대한 정보
     # new_label[0] = 1 인 경우, 0번 cluster는 1번 으로 label 변경
     
     for i in range(nk):
-        min_idx = distances_rest.argmin() # 전체 value 중, minimum value의 location
+        min_idx = dist_btween_centers.argmin() # 전체 value 중, minimum value의 location
         min_idx_i = min_idx // nk # min value가 위치한 row (i th cluster label in bootstrap data)
         min_idx_j = min_idx - min_idx_i * nk # min value가 위치한 column (j th cluster label in original data)
         
         if min_idx_j in new_label:
             # min_idx_j가 이미 등장한 경우, inf로 설정해줌으로써 min에 의해 탐지되지 않도록 함
-            distances_rest[min_idx_i, min_idx_j] = np.inf
-            min_idx = distances_rest.argmin()
+            dist_btween_centers[min_idx_i, min_idx_j] = np.inf
+            min_idx = dist_btween_centers.argmin()
             min_idx_i = min_idx // nk
             min_idx_j = min_idx - min_idx_i * nk
         
@@ -25,24 +29,24 @@ def explore_new_label(distances_rest, nk):
         # i-th cluster of boostrap data는 이미 배정됐으니, min 값 탐색 후보에서 제외
         # min 값 탐색 후보에서 제외하기 위하여 inf로 설정
         all_false[min_idx_i] = True
-        distances_rest[all_false] = np.inf 
+        dist_btween_centers[all_false] = np.inf 
     
     return new_label.astype(int)
 
-def map_B_center_to_O_center(org_data, O_centers, O_lables, B_centers, sim_method, nk):
+def map_B_center_to_O_center(org_data, O_centers, O_lables, B_centers, B2O_mapping_method, nk):
     """
         Bootstrap data로부터 구한 center를 Original data의 center에 matching 시키기
         
         sim_method에 따라서 mapping method가 다름
     """
-    if sim_method == 'euclidean':
+    if B2O_mapping_method == 'euclidean':
         distances = cdist(B_centers, O_centers, metric='euclidean') # distance matrix between two centers
         # for row i and column j,
         # [i, j] --> distance between i_th B_center and j_th O_center
-        distances_rest = distances.copy() # distances 복제
+        dist_btween_centers = distances.copy() # distances 복제
         
         
-    elif sim_method == 'jaccard':
+    elif B2O_mapping_method == 'jaccard':
          # original data와 B_centroids 중, 가장 거리가 짧은 centroid가 속한 cluster로 labeling
         o2b_labels = cdist(B_centers, org_data, metric='euclidean').argmin(axis=0)
         
@@ -61,9 +65,9 @@ def map_B_center_to_O_center(org_data, O_centers, O_lables, B_centers, sim_metho
                 distances[i, j] = -jaccard # matrix update
                 # minimum 값을 찾아가는 explore_new_label() 알고리즘에 맞추기 위하여 (-)를 취해줌
                 # jaccard는 값이 클수록 similarity가 크기 때문
-        distances_rest = distances.copy()
+        dist_btween_centers = distances.copy()
                 
-    new_label = explore_new_label(distances_rest = distances_rest, nk = nk)
+    new_label = explore_new_label(dist_btween_centers = dist_btween_centers, nk = nk)
     new_center = B_centers[new_label]
             
     return  new_center
@@ -80,5 +84,7 @@ class clustering_algs():
             _km.fit(data)
             
             self.center = _km.cluster_centers_
-            self.maaped_center = _km.cluster_centers_ * 0
-            self.labels = _km.labels_         
+            self.B2O_center = _km.cluster_centers_ * 0 # _bootClustering.B2O_center = mapped_center  을 통해 업데이트 되어야 함
+            # stability_scheme1.py의 line88 참고
+            self.labels = _km.labels_        
+            self.data = data 
